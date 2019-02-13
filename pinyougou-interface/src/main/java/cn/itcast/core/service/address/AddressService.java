4625abe488014c8a07731d25ccc7a06614fc3cfb
lgl <2904449185@qq.com>
2019-02-13 15:48:40 +0800

    地址管理的添加，删除接口实现

--- pinyougou-interface/src/main/java/cn/itcast/core/service/address/AddressService.java
+++ pinyougou-interface/src/main/java/cn/itcast/core/service/address/AddressService.java
@@ -6,4 +6,11 @@ import java.util.List;
 
 public interface AddressService {
     List<Address> findListByLoginUser(String username);
+
+
+    //地址添加
+    void add(Address address);
+
+    //地址删除
+    void del(Long[] ids);
 }
