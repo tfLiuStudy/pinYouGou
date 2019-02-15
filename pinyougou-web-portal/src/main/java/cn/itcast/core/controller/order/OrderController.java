7ff0858c0761b2c39ff748d5c75cf968f7629cd4
lgl <2904449185@qq.com>
2019-02-15 11:49:21 +0800

    查询所有的订单

--- pinyougou-web-portal/src/main/java/cn/itcast/core/controller/order/OrderController.java
+++ pinyougou-web-portal/src/main/java/cn/itcast/core/controller/order/OrderController.java
@@ -29,7 +29,7 @@ public class OrderController {
             return new Result(false,"提交订单失败");
         }
     }
-    //查询所有订单
+    //查询所有的订单
     @RequestMapping("/findAll.do")
     public List<Order> findAll(){
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
