2bffdb911b7d97495a4dc2a3e3bafddacd92c788
lgl <2904449185@qq.com>
2019-02-15 17:10:32 +0800

    地址管理

--- pinyougou-web-user/src/main/java/cn/itcast/core/controller/AddressController.java
+++ pinyougou-web-user/src/main/java/cn/itcast/core/controller/AddressController.java
@@ -6,32 +6,31 @@ import cn.itcast.core.service.address.AddressService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/address")
 public class AddressController {
-
     @Reference
     private AddressService addressService;
-    /**
-     * 地址管理之地址的添加
-     * @return
-     */
-    @RequestMapping("/add")
+@RequestMapping("/add.do")
     public Result add(@RequestBody Address address){
-        try {
-            addressService.add(address);
-            return new Result(true,"添加成功");
-        } catch (Exception e) {
-            return new Result(false,"添加失败");
-        }
+    try {
+        addressService.add(address);
+        return new Result(true,"添加成功");
+    } catch (Exception e) {
+        return new Result(false,"添加失败");
     }
-
-    @RequestMapping("del")
+}
+@RequestMapping("/del.do")
     public Result del(Long[] ids){
-        try {
-            addressService.del(ids);
-            return new Result(true,"删除成功");
-        } catch (Exception e) {
-            return new Result(false,"删除失败");
-        }
+    try {
+        addressService.del(ids);
+        return new Result(true,"删除失败");
+    } catch (Exception e) {
+        e.printStackTrace();
+        return new Result(false,"删除失败");
     }
+}
+
 }
